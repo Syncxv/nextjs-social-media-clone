176da8a066a9a953480880f5e718cfc4928bf5e0
Syncxv <aruldevarackal1@gmail.com>
2022-02-17 03:15:40 +0530

    post like query

--- src/apollo/queries/posts.ts
+++ src/apollo/queries/posts.ts
@@ -29,3 +29,61 @@ export const GET_POSTS_QUERY = gql`
         }
     }
 `
+
+export const GET_POST_QUERY = gql`
+    query GetPost($post_id: String!) {
+        getPost(post_id: $post_id) {
+            _id
+            title
+            content
+            attachment
+            likes
+            likedUsers
+            createdAt
+            updatedAt
+            owner {
+                _id
+                username
+                displayName
+                avatar
+                isStaff
+                createdAt
+                updatedAt
+            }
+            comments {
+                _id
+                content
+                likes
+                author {
+                    _id
+                    username
+                    displayName
+                    avatar
+                    isStaff
+                    createdAt
+                    updatedAt
+                }
+                createdAt
+                updatedAt
+            }
+        }
+    }
+`
+export const POST_LIKE_MUTATION = gql`
+    mutation LikePost($likePostPostId2: String!) {
+        likePost(post_id: $likePostPostId2) {
+            errors {
+                message
+            }
+            post {
+                _id
+                content
+                owner {
+                    _id
+                    username
+                }
+                likedUsers
+            }
+        }
+    }
+`

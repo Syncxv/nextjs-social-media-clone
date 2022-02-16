8bf80e9ee8f7a174ccd82d035676513213e62136
Syncxv <aruldevarackal1@gmail.com>
2022-02-17 00:53:59 +0530

    forgot to make index use layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import { Box, HStack, Grid, GridItem } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import SideBar from '../components/organism/Sidebar'
+import Layout from '../components/layout'
 import withAuth from '../components/withAtuh'
 
 const Home: NextPage = () => {
     return (
         <>
-            <HStack width="100vw" height="100vh">
-                <SideBar />
-                <Box backgroundColor="tomato">hi</Box>
-            </HStack>
+            <Layout>
+                <Box>hi</Box>
+            </Layout>
         </>
     )
 }

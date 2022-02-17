4713fc81fbd73e692e5e5a083dd41b34df74b488
Syncxv <aruldevarackal1@gmail.com>
2022-02-17 19:36:11 +0530

    :D i think this works pls

--- src/components/withAtuh.tsx
+++ src/components/withAtuh.tsx
@@ -6,43 +6,32 @@ import { UserType } from '../types'
 const useAuth = () => {
     const store = userStore()
     const [loading, setLoading] = useState(true)
-    const [data, setData] = useState<UserType | null>(null)
-    const [error, setError] = useState(false)
     useEffect(() => {
         store
             .initalize()
             .then(res => {
-                if (res.erros) {
-                    setError(true)
-                    setLoading(false)
-                } else {
-                    setData(store.user)
-                    setLoading(false)
-                }
+                setLoading(false)
             })
             .catch(err => {
                 setLoading(false)
-                setError(true)
             })
             .finally(() => {
                 setLoading(false)
-                setError(true)
             })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    return { data, loading, error }
+    return { loading, store }
 }
 const withAuth = <T extends object>(Component: React.FC<T> | NextPage<T>) => {
     // eslint-disable-next-line react/display-name
     return (props: any) => {
-        const { loading, error, data } = useAuth()
+        const { loading, store } = useAuth()
         const router = useRouter()
         if (loading) return <div>LOADING</div>
-        if (error || data === null) {
+        if (store.user === null) {
             router.push('/login')
             return <div>ERROR</div>
         }
-        console.log(loading, error, data)
         return <Component {...props} />
     }
 }

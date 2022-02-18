7ee23fa49766302fe5f3fdecff4cbf551f6a3323
Syncxv <aruldevarackal1@gmail.com>
2022-02-18 11:15:12 +0530

    SSR profile

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@apollo/client'
 import { Avatar, Box, Button, Flex, Heading, Text, Tooltip, Wrap } from '@chakra-ui/react'
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { Pencil, PencilSimple } from 'phosphor-react'
 import React from 'react'
 import styled from 'styled-components'
+import client from '../../apollo/client'
 import { GET_USER_BY_USERNAME_QUERY } from '../../apollo/queries/user'
 import Layout from '../../components/Layout'
 import { PostHeader } from '../../components/moluclues/Post'
@@ -13,22 +14,20 @@ import { useFollow } from '../../hooks/useFollow'
 import { userStore } from '../../stores/user'
 import { UserType } from '../../types'
 
-interface Props {}
+interface Props {
+    user: UserType
+}
 const ProfileAvatar = styled(Avatar)`
     --opacity: 0;
     transition: all 200ms ease;
 `
-const ProfilePage: NextPage<Props> = ({}) => {
+const ProfilePage: NextPage<Props> = ({ user }) => {
     const router = useRouter()
-    const { loading, data, error } = useQuery<{ findUser: UserType }>(GET_USER_BY_USERNAME_QUERY, {
-        variables: { username: router.query.username }
-    })
-    const { unfollow, follow, followers, isFollowing } = useFollow(data?.findUser)
+    const { unfollow, follow, followers, isFollowing } = useFollow(user)
     const store = userStore()!
-    if (loading) return <div>Loading</div>
-    if (!data?.findUser || error) return <div>Not found eh</div>
-    const { findUser: user } = data
+    if (!user) return <div>not found eh</div>
     const isCurrentUser = user._id === store.user!._id
+    console.log(user)
     return (
         <Layout>
             <PostHeader label={user.displayName} />
@@ -84,5 +83,15 @@ const ProfilePage: NextPage<Props> = ({}) => {
         </Layout>
     )
 }
-
+export const getServerSideProps: GetServerSideProps = async context => {
+    const {
+        data: { findUser }
+    } = await client.query<{ findUser: UserType }>({
+        query: GET_USER_BY_USERNAME_QUERY,
+        variables: { username: context.query.username }
+    })
+    return {
+        props: { user: findUser }
+    }
+}
 export default withAuth(ProfilePage)

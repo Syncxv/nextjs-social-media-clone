c44aa15738f26a09465842f1eb9e0bbfe1176ab0
Syncxv <aruldevarackal1@gmail.com>
2022-02-18 10:42:08 +0530

    sheeesh

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client'
-import { Avatar, Box, Button, Flex, Heading, Text, Wrap } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, Heading, Text, Tooltip, Wrap } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { Pencil, PencilSimple } from 'phosphor-react'
@@ -7,28 +7,33 @@ import React from 'react'
 import styled from 'styled-components'
 import { GET_USER_BY_USERNAME_QUERY } from '../../apollo/queries/user'
 import Layout from '../../components/Layout'
+import { PostHeader } from '../../components/moluclues/Post'
+import withAuth from '../../components/withAtuh'
+import { userStore } from '../../stores/user'
 import { UserType } from '../../types'
 
 interface Props {}
 const ProfileAvatar = styled(Avatar)`
     --opacity: 0;
     transition: all 200ms ease;
-    &:hover {
-        --opacity: 1;
-        filter: brightness(70%);
-    }
 `
 const ProfilePage: NextPage<Props> = ({}) => {
     const router = useRouter()
     const { loading, data, error } = useQuery<{ findUser: UserType }>(GET_USER_BY_USERNAME_QUERY, {
         variables: { username: router.query.username }
     })
+    const store = userStore()!
     console.log(data)
     if (loading) return <div>Loading</div>
     if (!data?.findUser || error) return <div>Not found eh</div>
     const { findUser: user } = data
+    const isCurrentUser = user._id === store.user!._id
+    const follow = () => {
+        console.log('HEHEHE HA')
+    }
     return (
         <Layout>
+            <PostHeader label={user.displayName} />
             <Box height="20rem" width="100%">
                 <Box height="50%" backgroundColor="#ff5e5e"></Box>
                 <Flex pl={5} direction="column" className="user-stuff">
@@ -38,8 +43,11 @@ const ProfilePage: NextPage<Props> = ({}) => {
                         name={user.username}
                         src={user.avatar}
                         position="absolute"
-                        _hover={{ filter: 'brightness(70%)' }}
-                        onClick={() => router.push('/settings/profile')}
+                        _hover={{
+                            '--opacity': isCurrentUser ? 1 : 0,
+                            filter: isCurrentUser ? 'brightness(70%)' : 'none'
+                        }}
+                        onClick={() => isCurrentUser && router.push('/settings/profile')}
                     >
                         <Flex
                             justifyContent="center"
@@ -52,8 +60,12 @@ const ProfilePage: NextPage<Props> = ({}) => {
                         </Flex>
                     </ProfileAvatar>
                     <Flex m={2} justifyContent="flex-end">
-                        <Button colorScheme="blue" _focus={{ boxShadow: 'none' }}>
-                            Follow
+                        <Button
+                            colorScheme="blue"
+                            _focus={{ boxShadow: 'none' }}
+                            onClick={() => (isCurrentUser ? router.push('/settings/profile') : follow())}
+                        >
+                            {isCurrentUser ? 'Edit Profile' : 'Follow'}
                         </Button>
                     </Flex>
                     <Box>
@@ -69,4 +81,4 @@ const ProfilePage: NextPage<Props> = ({}) => {
     )
 }
 
-export default ProfilePage
+export default withAuth(ProfilePage)
